1a7a30654528b4129681afdad1c2b3c48e5dbc59
Jitesh Gaikwad <gaikwadjitesh42@gmail.com>
2022-03-31 17:32:48 +0530

    v1.3.1.2 dropdown choices added to Logger

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Bot Framework v4 adaptive runtime package sample.
 
 This is an example of a package that can be consumed by the new adaptive runtime.
+
+Custom Actions Developed so far:
+- Logger Dialog 
+- Addition Dialog
--- src/loggerDialog.ts
+++ src/loggerDialog.ts
@@ -5,6 +5,8 @@ import {
     Expression,
     StringExpression,
     StringExpressionConverter,
+    EnumExpression,
+    EnumExpressionConverter
 } from "adaptive-expressions";
 
 import {
@@ -13,12 +15,22 @@ import {
     Dialog,
     DialogConfiguration,
     DialogContext,
-    DialogTurnResult,
+    DialogTurnResult
 } from "botbuilder-dialogs";
+
+
+export enum LogType {
+    plaintext = 'plaintext',
+    masked = 'masked',
+    encrypted = 'encrypt'
+}
 
 export interface LoggerDialogConfiguration extends DialogConfiguration {
     message?: string | Expression | StringExpression;
+    logType?: string | Expression | EnumExpression<LogType>;
 }
+
+
 
 export class LoggerDialog
     extends Dialog
@@ -26,6 +38,18 @@ export class LoggerDialog
     public static $kind = "LoggerDialog";
     // public message?: StringExpression;
     public message: StringExpression = new StringExpression('');
+    public logType: EnumExpression<LogType> = new EnumExpression<LogType>(LogType.plaintext);
+
+
+    public constructor(message: string, logType?: LogType) {
+        super();
+        if (logType) {
+            this.logType = new EnumExpression<LogType>(logType);
+        }
+        if (message) {
+            this.message = new StringExpression(message);
+        }
+    }
 
     public getConverter(
         property: keyof LoggerDialogConfiguration
@@ -33,6 +57,8 @@ export class LoggerDialog
         switch (property) {
             case "message":
                 return new StringExpressionConverter();
+            case "logType":
+                return new EnumExpressionConverter<LogType>(LogType);
             default:
                 return super.getConverter(property);
         }
@@ -51,11 +77,17 @@ export class LoggerDialog
     }
     public beginDialog(dc: DialogContext): Promise<DialogTurnResult> {
         console.log('LoggerDialog Called');
-        console.log(this.message);
+        // console.log(this.message);
+        // console.log(this.logType);
         const _message = this.message?.getValue(dc.state);
-        console.log(_message)
-        console.log(this.emailMask(_message))
-        // console.log(this.emailMask(_message));
+        const _logType = this.logType?.getValue(dc.state);
+        console.log(`Logtype: ${_logType}`)
+        if (_logType == LogType.masked) {
+            console.log(this.emailMask(_message))
+        }
+        else {
+            console.log(_message)
+        }
         return dc.endDialog();
     }
 
